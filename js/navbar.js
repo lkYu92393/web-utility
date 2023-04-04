4dc83568c74e35c1431041ccbd395710f541629a
gundam1945 <gundam1945@hotmail.com>
2023-04-04 15:36:03 +0800

    change to structure

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.add("hidden");
+    
+    let funDiv = document.createElement('div');
+    navbar.appendChild(funDiv);
+})
